db24b813a1402b518d9cc60c65f2ac1d95e864c5
Darasimi 2003 <owofadegeorge@gmail.com>
2024-04-16 04:13:30 +0100

    My list

---
+++ my_list.py
@@ -0,0 +1,23 @@
+# Define an empty list called my_list
+my_list = []
+
+# Append elements to my_list: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position in my_list
+my_list.insert(1, 15)
+
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Print my_list to verify the final result
+print(my_list)
